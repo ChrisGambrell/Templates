0b71791de415a3b6879859d08c8c4bdeec6691bd
Chris Gambrell <christopher@gambrell.info>
2021-07-11 21:46:27 -0400

    Add /auth/login route

--- resources/flask/flaskr/auth.py
+++ resources/flask/flaskr/auth.py
@@ -2,9 +2,40 @@
 
 from flask import Blueprint, jsonify, request
 from flaskr.db import get_db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json() if request.get_json() is not None else {}
+    username = data.get('username', None)
+    password = data.get('password', None)
+
+    error = None
+
+    if not username:
+        error = 'Username is required.'
+    elif not password:
+        error = 'Password is required.'
+
+    if error:
+        return jsonify({'error': error})
+
+    db = get_db()
+    user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()
+    user_data = {key: user[key] for key in user.keys()} if user is not None else None
+
+    if user_data is None:
+        error = 'Username is incorrect.'
+    elif not check_password_hash(user_data.get('password', ''), password):
+        error = 'Password is incorrect.'
+
+    if error:
+        return jsonify({'error': error})
+
+    return jsonify(user_data)
 
 
 @bp.route('/register', methods=['POST'])
@@ -26,7 +57,7 @@ def register():
     elif db.execute('SELECT id FROM user WHERE username = ?', (username,)).fetchone() is not None:
         error = f'Username {username} is taken.'
 
-    if error is not None:
+    if error:
         return jsonify({"error": error})
 
     db.execute('INSERT INTO user (name, username, password) VALUES (?, ?, ?)', (name, username, generate_password_hash(password)))
--- resources/flask/tests/test_auth.py
+++ resources/flask/tests/test_auth.py
@@ -3,6 +3,19 @@
 import pytest
 
 
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', 'Username is required.'),
+    ('username', '', 'Password is required.'),
+    ('user', 'password', 'Username is incorrect.'),
+    ('username', 'pass', 'Password is incorrect.'),
+    ('username', 'password', '')
+))
+def test_login_validate_input(client, username, password, message):
+    response = client.post('/auth/login', json={'username': username, 'password': password})
+    data = response.get_json() if response.get_json() is not None else {}
+    assert message in data.get('error', '')
+
+
 @pytest.mark.parametrize(('name', 'username', 'password', 'message'), (
     ('', '', '', 'Name is required.'),
     ('user', '', '', 'Username is required.'),
