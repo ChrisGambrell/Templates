0a75c901b4435685e89cdf1428859724f228d167
Chris Gambrell <christopher@gambrell.info>
2021-11-25 12:45:00 -0600

    Don't lint root files of endpoints

--- flaskr/auth/__init__.py
+++ flaskr/auth/__init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+# flake8: noqa
+
 from cerberus import Validator
 from flask import Blueprint
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 v = Validator(purge_unknown=True, require_all=True)
 
-from . import login     # noqa: E402, F401
-from . import register  # noqa: E402, F401
+from . import login
+from . import register
--- flaskr/tasks/__init__.py
+++ flaskr/tasks/__init__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
+# flake8: noqa
+
 from cerberus import Validator
 from flask import Blueprint
 
 bp = Blueprint('tasks', __name__, url_prefix='/tasks')
 v = Validator(purge_unknown=True)
 
-from . import create_task       # noqa: E402, F401
-from . import delete_task       # noqa: E402, F401
-from . import edit_task         # noqa: E402, F401
-from . import get_task_by_id    # noqa: E402, F401
-from . import get_tasks         # noqa: E402, F401
+from . import create_task
+from . import delete_task
+from . import edit_task
+from . import get_task_by_id
+from . import get_tasks
